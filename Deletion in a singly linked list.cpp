0d37b90cab8c443512ba01f9bbbbd2b57a2f69ce
190101150003 <96740178+190101150003@users.noreply.github.com>
2022-02-28 16:22:20 +0530

    Add files via upload

---
+++ Deletion in a singly linked list.cpp
@@ -0,0 +1,90 @@
+// deletion in singly linked list with class
+#include <bits/stdc++.h>
+# include<iostream>
+using namespace std;
+
+// A linked list node
+class Node{
+public:
+	int data;
+	Node* next;
+};
+void push(Node** head_ref, int new_data)
+{
+	Node* new_node = new Node();
+	new_node->data = new_data;
+	new_node->next = (*head_ref);
+	(*head_ref) = new_node;
+}
+void deleteNode(Node** head_ref, int key)
+{
+	
+	// Store head node
+	Node* temp = *head_ref;
+	Node* prev = NULL;
+	
+	
+	if (temp != NULL && temp->data == key)
+	{
+		*head_ref = temp->next; // Changed head
+		delete temp;		 // free old head
+		return;
+	}
+
+	else
+	{
+	while (temp != NULL && temp->data != key)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+
+	// If key was not present in linked list
+	if (temp == NULL)
+		return;
+
+	// Unlink the node from linked list
+	prev->next = temp->next;
+
+	// Free memory
+	delete temp;
+	}
+}
+
+// linked list starting from the
+
+void printList(Node* node)
+{
+	while (node != NULL)
+	{
+		cout << node->data << " ";
+		node = node->next;
+	}
+}
+
+// Driver code
+int main()
+{
+	
+	// Start with the empty list
+	Node* head = NULL;
+
+	// Add elements in linked list
+	push(&head, 7);
+	push(&head, 1);
+	push(&head, 3);
+	push(&head, 2);
+
+	puts("Created Linked List: ");
+	printList(head);
+
+	deleteNode(&head, 7);
+	puts("\nLinked List after Deletion of 7: ");
+	
+	printList(head);
+	
+	return 0;
+}
+
+
+
